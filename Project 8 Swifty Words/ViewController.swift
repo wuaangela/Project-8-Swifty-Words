ac7e08db288f1ee0ff8dcda047d7af44cc18bd1e
A W <waq.mmhy@live.ca>
2019-07-03 11:40:29 -0400

    Initial Commit

---
+++ Project 8 Swifty Words/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 8 Swifty Words
+//
+//  Created by Angela Wu  on 2019-07-03.
+//  Copyright © 2019 Angela Wu . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
